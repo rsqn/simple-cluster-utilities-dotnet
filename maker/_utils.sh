02e7a1552ced79730b379f427f43af7e29317542
Michael Andrewes <mandrewes@bellpotter.com.au>
2024-11-14 15:48:46 +1100

    none

--- maker/_utils.sh
+++ maker/_utils.sh
@@ -1,14 +1,2 @@
 #
 
-function getarg() {
-  local name="$1"
-  shift  # Remove the name argument from the list
-
-  for arg in "$@"; do
-    if [[ "$arg" == "--"$name"="* ]]; then
-      echo "${arg#*=}"
-      return 0
-    fi
-  done
-  return 1
-}
